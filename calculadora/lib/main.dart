79994646548ff29d95b889ae05b94b9357561d2d
Skip0s <61255262+SkipOs@users.noreply.github.com>
2024-07-03 19:29:43 -0300

    Update main.dart

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -108,7 +108,7 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
+        title: Text(
           'Calculadora',
           style: TextStyle(color: Colors.white),
         ),
